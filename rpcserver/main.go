5b115697037d6e38bf3c9b66b40242ddc521b6ec
thomas92911 <thomas92911@gmail.com>
2019-04-26 22:17:31 +0800

    Update main.go

--- rpcserver/main.go
+++ rpcserver/main.go
@@ -7,18 +7,7 @@ import (
 
 	"github.com/gorilla/rpc"
 	"github.com/gorilla/rpc/json"
-	"github.com/thomas92911/go-utils"
 )
-
-// GraphQL
-// https://graphql.cn/code/#go
-//
-// MacOS Rest Client
-// https://github.com/mmattozzi/cocoa-rest-client/releases
-//
-// openssl genrsa -out server.key 2048
-// openssl req -nodes -new -key server.key -subj "/CN=localhost" -out server.csr
-// openssl x509 -req -sha256 -days 888888 -in server.csr -signkey server.key -out server.crt
 
 // CallArgs define
 type CallArgs struct {
@@ -37,13 +26,11 @@ type CallService struct{}
 func (h *CallService) Call(r *http.Request, args *CallArgs, reply *CallReply) error {
 	reply.Message = "Hello, " + args.Who + "!"
 	return nil
-	//return errors.New("error haha")
 }
 
 func init() {
 	bs, err := json.EncodeClientRequest("CallService.Call", &CallArgs{"Merak"})
 	log.Println(string(bs), err)
-	// {"method":"CallService.Call","params":[{"Who":"Merak"}],"id":5577006791947779410}
 
 	s := rpc.NewServer()
 	s.RegisterCodec(json.NewCodec(), "application/json")
@@ -54,10 +41,6 @@ func init() {
 }
 
 func main() {
-
-	port, err := utils.GetUnUsedPort()
-	log.Println(port, err)
-
 	for {
 		time.Sleep(time.Second)
 	}
